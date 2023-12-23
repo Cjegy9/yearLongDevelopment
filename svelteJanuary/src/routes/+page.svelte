4c2154766def59200cc0fdf6dc8ba01216811820
Colin Eyster <eystercolin@gmail.com>
2023-12-23 14:16:03 -0600

    trying to figure out why people like this

--- svelteJanuary/src/routes/+page.svelte
+++ svelteJanuary/src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+ export let readonlyvar = 'This is readonly';
+
+ console.log({readonlyvar});
+ $: {
+  console.log({readonlyvar})
+ }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={() => readonlyvar = 'not so read only now'}>Update the readonlyvar</button>
+<p>{readonlyvar}</p>
