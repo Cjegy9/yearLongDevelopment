43b7e09680faf7d969f2f47205b42eb668060d68
Colin Eyster <eystercolin@gmail.com>
2024-01-03 21:37:17 -0600

    starting a nav bar

--- svelteJanuary/src/routes/+layout.svelte
+++ svelteJanuary/src/routes/+layout.svelte
@@ -3,8 +3,46 @@
  
 </script>
 
-<div>
-  wrapping every page
+<div class="layout">
+  <ul class="horizontal">
+    <li>
+      <a>Home</a>
+    </li>
+    <li>
+      <a>About</a>
+    </li>
+    <li>
+      <a>Settings</a>
+    </li>
+    <li class="float-right" style="float: right">
+      <a>Profile</a>
+    </li>
+  </ul>
 </div>
 
 <slot></slot>
+
+<style>
+  .layout {
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #DADADA;
+  }
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  ul.horizontal li {
+    float: left;
+  }
+  ul.horizontal li a {
+    display: inline-block;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  .float-right {
+    float: right;
+  }
+</style>
